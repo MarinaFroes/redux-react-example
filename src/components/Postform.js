1ccebcd492934f1ef0450c8357f2dd8154ea959c
MFAC <ninafroes@gmail.com>
2019-07-31 10:05:58 +0200

    feat: Add simple form structure

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -4,7 +4,22 @@ class PostForm extends Component {
   render() {
     return (
       <div>
-        <h1>Posts</h1>
+        <h1>Add post</h1>
+        <form action="">
+          <div>
+            <label htmlFor="title">Title:</label>
+            <br />
+            <input type="text" name="title" id=""/>
+          </div>
+          <br />
+          <div>
+            <label htmlFor="body">Body:</label>
+            <br />
+            <textarea name="body" id="" cols="30" rows="10"></textarea>
+          </div>
+          <br />
+          <button type="submit">Submit</button>
+        </form>
       </div>
     )
   }
