19ec96ef5f501f229114062317859d02a305ab23
MFAC <ninafroes@gmail.com>
2019-07-31 11:25:03 +0200

    feat: Add provide

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
+
 
 import Posts from './components/Posts';
 import Postform from './components/Postform';
+import store from './components/store';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Provider store={store}>
+      <div className="App">
         <Postform />
         <hr />
         <Posts />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </Provider>
   );
 }
 
