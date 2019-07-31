e1d0e418855b84f8c509552de85e9472b5729da2
MFAC <ninafroes@gmail.com>
2019-07-31 10:06:09 +0200

    feat: Import Postform

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Posts from './components/Posts'
+import Posts from './components/Posts';
+import Postform from './components/Postform';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Postform />
+        <hr />
         <Posts />
         <a
           className="App-link"
