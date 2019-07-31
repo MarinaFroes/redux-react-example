8d3d55c40867a33617188d5b2eed16cdd25d6dce
MFAC <ninafroes@gmail.com>
2019-07-31 11:03:08 +0200

    feat: Add handleSubmit and handleChange

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -1,21 +1,50 @@
 import React, { Component } from 'react'
 
 class PostForm extends Component {
+  state = {
+    title: '',
+    body: ''
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const post = {
+      title: this.state.title,
+      body: this.state.body
+    }
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(post)
+    })
+      .then(res => res.json())
+      .then(data => console.log(data));
+  }
+
   render() {
     return (
       <div>
         <h1>Add post</h1>
-        <form action="">
+        <form onSubmit={this.handleSubmit}>
           <div>
             <label htmlFor="title">Title:</label>
             <br />
-            <input type="text" name="title" id=""/>
+            <input type="text" name="title" value={this.state.title} onChange={this.handleChange}/>
           </div>
           <br />
           <div>
             <label htmlFor="body">Body:</label>
             <br />
-            <textarea name="body" id="" cols="30" rows="10"></textarea>
+            <textarea name="body" id="" cols="30" rows="10" value={this.state.body} onChange={this.handleChange}></textarea>
           </div>
           <br />
           <button type="submit">Submit</button>
